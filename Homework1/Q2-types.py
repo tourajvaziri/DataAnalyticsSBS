ef3e43abce1ab18cc1bd987c68cb1da075a0b5c1
touraj vaziri <vtouraj@gmail.com>
2024-01-21 09:43:45 +0100

    Create Q2-types.py

---
+++ Homework1/Q2-types.py
@@ -0,0 +1,9 @@
+# Create different variables that hold different types:
+# int, float, str, bool and datetime
+
+# The using the variables perform different operations, for reference on possible operations check: ****
+# 1. int: i.e. add, multiplication
+# 2. float: i.e. division, finding the floor (3.3 -> 3)
+# 3. str: i.e. replace, characters count ('hello' -> 5), to lowercase ('BMW' -> 'bmw')
+# 4. bool: i.e. compare two variables (a == b -> True)
+# 5. datetime: create a datetime representation of your date of birth.  (Note: For working with datetime you first to need import it)

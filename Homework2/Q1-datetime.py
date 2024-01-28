16e53c3b96e2ffea4a81da70aef0ef81b8cbf9ef
touraj <tvaziri@netsuite.com>
2024-01-28 19:21:17 +0100

    adding homework2

---
+++ Homework2/Q1-datetime.py
@@ -0,0 +1,2 @@
+# Create two variables of type datetime with values of your choice for year, month, day etc. Then ouput the difference in years between the two variables.
+# i.e.   2018,02,01  &  2016,01,03   ->  2         (2018-2016)
---
+++ Homework2/Q2-conditions.py
@@ -0,0 +1,6 @@
+# Define two variables of type integer and give them values of your choice. 
+# Then, write a conditional code:
+# If the two variables are equal: prints ‘The variables are equal’
+# If variable1 is double than variable2: prints: ‘The variable1 is double than variable2’
+# If variable2 is double than variable1: prints: ‘The variable2 is double than variable1’
+# And in any other case: prints the difference between them

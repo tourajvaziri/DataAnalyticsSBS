6dc38a4f191a6b5e02082f63f443fc0e08416fb4
eyram awudi <eyram@sbs.barcelona>
2024-02-09 19:42:33 +0100

    Q3 answered

--- Homework3/Q3.py
+++ Homework3/Q3.py
@@ -1,2 +1,3 @@
 # Write code to print all the lines in the file carData.csv. (Check current directory for the file) 
 # Note: Call file.close() after your code to properly close the resource
+my answer

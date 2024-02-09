21d58e07a090cec38c38c4979d63dd3804656c76
AndreaASBS <157827796+AndreaASBS@users.noreply.github.com>
2024-02-09 19:39:58 +0100

    Update Q1.py

--- Homework3/Q1.py
+++ Homework3/Q1.py
@@ -1,2 +1,11 @@
 # Write code that prints the current time in this format: (hour:minutes:seconds)
 # example:  15:58:19
+
+In [1]: from datetime import datetime
+
+In [2]: now = datetime.now()
+
+In [3]: current_time = now.strftime("%H:%M:%S")
+
+In [4]: current_time
+Out[4]: '15:42:52'

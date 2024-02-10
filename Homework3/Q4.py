748051fac24b94c50f0c26affd2df7240ccee829
eyramsbs <157289090+eyramsbs@users.noreply.github.com>
2024-02-10 04:52:09 +0100

    Update Q4.py

--- Homework3/Q4.py
+++ Homework3/Q4.py
@@ -1,3 +1,12 @@
 # Write code to output the count of emails by source (Yahoo or Gmail) and overall count (combined).
 # Example data: ‘sameer@yahoo.com, SUE@GMAIL.com, eyram@gmail.com, Andrea@Yahoo.com,.....’
 # Output: ‘Yahoo Emails: 2, Gmail Emails: 2, Total Emails: 4’
+>>> emailsData = 'sameer@yahoo.com,sue@GMAIL.com,Andrea@gmail.com,Eyram@Yahoo.com'
+>>> emailsData = emailsData.lower()
+>>> emailsData.count('gmail')
+2
+>>> emailsData.count('yahoo')
+2
+>>> print("Number of gmail accounts is 2, Number of yahoo accounts is 2 and total number of emails being4")
+Number of gmail accounts is 2, Number of yahoo accounts is 2 and total number of emails being4
+>>> 

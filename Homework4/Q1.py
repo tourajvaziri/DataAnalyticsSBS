fd1fe12c178959f00ce3341e333e6c9d30de1b74
AndreaASBS <157827796+AndreaASBS@users.noreply.github.com>
2024-02-16 13:36:39 +0100

    Update Q1.py

--- Homework4/Q1.py
+++ Homework4/Q1.py
@@ -1,7 +1,13 @@
 # Create four variables of type tuple that contain two pieces of information: vegetable name + it's country of origin
 # The vegetables are: Zucchini (Croatia), Tomato (Zimbawe), Pepper (India) and Onion (Ghana)
 
+In [1]: zucchini = ("Zucchini","Croatia")
+In [2]: tomato = ("Tomato","Zimbabwe")
+In [3]: pepper = ("Pepper","India")
+In [4]: onion = ("Onion","Ghana")
 
 # Then using the tuple variable, 
-
 # 1. print out the country of origin for Tomato
+
+In [5]: print(tomato[1])
+Zimbabwe
